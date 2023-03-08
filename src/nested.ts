1afc68b7ae4cb568e06f19f540f2d9e55b4220bd
Faith Lovell <faithlxvell@gmail.com>
2023-03-07 19:30:24 -0500

    fix git issue

--- src/nested.ts
+++ src/nested.ts
@@ -1,5 +1,3 @@
-import { queries } from "@testing-library/react";
-import { isQuestion } from "./functions";
 import { Answer } from "./interfaces/answer";
 import { Question, QuestionType } from "./interfaces/question";
 import { duplicateQuestion, makeBlankQuestion } from "./objects";
