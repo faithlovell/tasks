f73d428fd9e737b8bbb6d0a639e905218439ef18
Faith Lovell <faithlxvell@gmail.com>
2023-03-20 21:16:06 -0400

    edit mode

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,56 @@
 import React, { useState } from "react";
+import { Col, Form, Row } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    const [canEdit, setCanEdit] = useState<boolean>(false);
+    const [userName, setUserName] = useState<string>("Your Name");
+    const [isStudent, setIsStudent] = useState<boolean>(true);
+
+    function updateEdit(event: React.ChangeEvent<HTMLInputElement>) {
+        setCanEdit(event.target.checked);
+    }
+
+    function updateStudent(event: React.ChangeEvent<HTMLInputElement>) {
+        setIsStudent(event.target.checked);
+    }
+
     return (
         <div>
-            <h3>Edit Mode</h3>
+            <Form.Check
+                type="switch"
+                id="can-edit-check"
+                label="Edit"
+                checked={canEdit}
+                onChange={updateEdit}
+            />
+            <div>
+                {canEdit ? (
+                    <>
+                        <Form.Group controlId="formUserName">
+                            {" "}
+                            <Form.Label>Name:</Form.Label>{" "}
+                            <Form.Control
+                                value={userName}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                ) => setUserName(event.target.value)}
+                            />{" "}
+                        </Form.Group>{" "}
+                        <Form.Check
+                            type="checkbox"
+                            id="is-student-checkbox"
+                            label="Are you a student?"
+                            checked={isStudent}
+                            onChange={updateStudent}
+                        />{" "}
+                    </>
+                ) : (
+                    <span>
+                        {" "}
+                        {userName} is {!isStudent && "not"} a student{" "}
+                    </span>
+                )}
+            </div>
         </div>
     );
 }
